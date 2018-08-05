21e40fe19bb30d4630e5e104bafef240b62ff896
Royce <2816708306@qq.com>
2018-08-05 09:31:06 +0800

    feat: Add assets resources

--- src/config.js
+++ src/config.js
@@ -1,1 +1,11 @@
 export const BASE_API = 'https://cnodejs.org/api/v1';
+export default {
+  maxImageSize: 1024 * 1024 * 3,
+  maxBackgroundImageSize: 1024 * 1024 * 3,
+
+  // 系统设置
+  primaryColor: '74, 144, 226',
+  primaryTextColor: '247, 247, 247',
+  backgroundImage: require('./assets/images/background.jpg'),
+  sound: 'default'
+};
